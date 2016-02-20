3c6d1cac8c07a9afc93a923adf1992d2f3cb39cd
Pjotr Prins <pjotr.public01@thebird.nl>
2016-02-20 14:02:02 +0000

    Added pylmm_gn2 to lib

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
       author_email='nick.furlotte@gmail.com',
       url = "https://github.com/nickFurlotte/pylmm",
       description = 'pyLMM is a lightweight linear mixed model solver for use in GWAS.',
-      packages=['pylmm'],
+      packages=['pylmm','pylmm_gn2'],
       scripts=['scripts/pylmmGWAS.py','scripts/pylmmKinship.py','scripts/pylmm_redis'],
     )
